27936f2c8367b339b6b35c2913028ec5c8ee4ef2
smartjune <yuanzhongyan@gmail.com>
2016-03-07 19:12:11 +0800

    1. 为Node添加pre属性，插入的同时统计，取消keysWithPrefix()和collect()方法； 2. 预先new出足够的Trie树节点，取代之前在put()中一个一个地new； 3. 不需要val属性

--- src/smartjune/P1014.java
+++ src/smartjune/P1014.java
@@ -1,24 +1,20 @@
 package smartjune;
-
-
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class P1014 {
-	private static int N;  // 词典大小
+	private static int N;  // 词条个数
 	private static int T;  // 询问次数
-	
-	private static int R = 256;
+	private static int R = 26;  // 儿子分支数
 	private static Node root;
+	private static Node[] nodes = new Node[1000000];  // 预先new出足够多的 Trie树节点
+	private static int cursor = 0;
 	
 	// 对应 P733 的三种情况
 	private static Node get(Node y, String key, int d) {
 		if (y == null)	return null;
 		if (d == key.length())	return y;
 		char c = key.charAt(d);
-		return get(y.next[c], key, d + 1);  // next[c] 即 next[115]			
+		return get(y.next[c - 97], key, d + 1);  // next[c] 即 next[115]			
 	}
 	
 	public static void put(String key, int val) {
@@ -27,42 +23,37 @@ public class P1014 {
 
 	// 对应 P734 的两种情况
 	private static Node put(Node x, String key, int val, int d) {
-		if (x == null)	x = new Node();
+		if (x == null)	
+			x = nodes[cursor++];
+		
+		x.pre++;
 		if (d == key.length()) {
-			x.val = val;
-			x.dup++;
 			return x;
 		}
+		
 		char c = key.charAt(d);
-		x.next[c] = put(x.next[c], key, val, d + 1);
+		x.next[c - 97] = put(x.next[c - 97], key, val, d + 1);
 		return x;
 	}
 		
-	public static Iterable<String> keysWithPrefix(String pre){
-		Queue<String> q = new LinkedList<String>();
-		Node arg1 = get(root, pre, 0);
-		collect(arg1, pre, q);
-		return q;
-	}
-	
-	private static void collect(Node x, String key, Queue<String> q) {
-		if (x == null)	return;
-		if (x.val != -1)
-			for (int i = 0; i < x.dup; i++)
-				q.add(key);
-		for (char c = 0; c < R; c++)
-			collect(x.next[c], key + c, q);
+	public static int numOfKeysWithPrefix(String pre) {
+		Node x = get(root, pre, 0);
+		if (x != null)		return x.pre;
+		return 0;
 	}
 
 	private static class Node {
-		private int val;  // 如 "apple" 是第一个插入的，其 val 就为 1
+		private int pre = 0;  // 以“从根节点到该节点构成的字符串”为前缀的单词的个数
 		private Node[] next = new Node[R];  // all links
-		private int dup = 0;  // 对应单词的重复次数
 	}
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		String key;
+		
+		for (int i = 0; i < nodes.length; i++) {
+			nodes[i] = new Node();
+		}
 		
 		N = scanner.nextInt();		
 		for (int i = 0; i < N; i++) {
@@ -73,12 +64,11 @@ public class P1014 {
 		T = scanner.nextInt();
 		for (int i = 0; i < T; i++) {
 			key = scanner.next();
-			List<String> it = (List)keysWithPrefix(key);
-			
-			System.out.println(it.size());
+			System.out.println(numOfKeysWithPrefix(key));
 		}
 		
 		scanner.close();
 	}
 	
 }
+
