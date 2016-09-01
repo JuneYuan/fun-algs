6b0eea2ea645ff6423072998aa9121f2f73225ce
smartjune <yuanzhongyan@gmail.com>
2016-09-01 20:18:08 +0800

    P223 Rectangle Area 求两矩形覆盖的面积

---
+++ src/leetcode/math/P223.java
@@ -0,0 +1,22 @@
+package leetcode.math;
+
+public class P223 {
+
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        int intersectionArea = 0;
+        
+        int left = Math.max(A, E);
+        int right = Math.min(C, G);
+        int top = Math.min(D, H);
+        int bottom = Math.max(B, F);
+        if (left <= right && bottom <= top) {
+            intersectionArea = (right - left) * (top - bottom);
+        }
+        
+        int rectArea1 = Math.abs(C - A) * Math.abs(D - B);
+        int rectArea2 = Math.abs(G - E) * Math.abs(H - F);
+        int ans = rectArea1 + rectArea2 - intersectionArea;
+        return ans;
+    }
+
+}
