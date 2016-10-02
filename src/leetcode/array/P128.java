06207ee775bd1eed8ab253f0ed1bc27c4974c891
smartjune <yuanzhongyan@gmail.com>
2016-10-02 23:59:29 +0800

    P128 Longest Consecutive Sequence 最大连续子数组

---
+++ src/leetcode/array/P128.java
@@ -0,0 +1,38 @@
+package leetcode.array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// 最大连续子数组
+public class P128 {
+    public int longestConsecutive(int[] A) {
+        if (A == null || A.length == 0)  return 0;
+        
+        // add number to hashset
+        Set<Integer> hashset = new HashSet<Integer>();
+        for (int a : A) {
+            hashset.add(a);
+        }
+        
+        int lcs = 0;
+        for (int a : A) {
+            int i = a, cnt = 1;
+            
+            hashset.remove(a);
+            // i--
+            while (hashset.contains(--i)) {
+                cnt++;
+                hashset.remove(i);
+            }
+            // i++
+            while (hashset.contains(++i)) {
+                cnt++;
+                hashset.remove(i);
+            }
+            // update lcs
+            lcs = Math.max(lcs, cnt);
+        }
+        
+        return lcs;
+    }
+}
