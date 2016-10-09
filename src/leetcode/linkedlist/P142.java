ec7e6c62eba877614d0bca66fda17edc6f54cfa3
smartjune <yuanzhongyan@gmail.com>
2016-10-09 11:36:21 +0800

    P141 Linked List Cycle

---
+++ src/leetcode/linkedlist/P142.java
@@ -0,0 +1,33 @@
+package leetcode.linkedlist;
+
+public class P142 {
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        // find Collision Spot
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        
+        // check if there is a cycle
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        
+        // move slow to head, and fast remains, so that they are at the same distance to cycle-start-node
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        return fast;
+    }
+
+}
