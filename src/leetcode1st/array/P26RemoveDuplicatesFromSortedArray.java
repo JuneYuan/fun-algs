0175a78f37497161b012b1543b30b3c6c8562305
yuanzhongyan <yuanzhongyan@xiaomi.com>
2018-06-07 22:46:32 +0800

    solve removeDuplicatesFromSortedArray

---
+++ src/leetcode1st/array/P26RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,32 @@
+package leetcode1st.array;
+
+import java.util.Arrays;
+
+public class P26RemoveDuplicatesFromSortedArray {
+
+    public int solution(int[] nums) {
+        if (nums == null) {
+            return -1;
+        }
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+
+        int newIndex = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[newIndex]) {
+                newIndex++;
+                nums[newIndex] = nums[i];
+            }
+        }
+
+        return newIndex + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 1, 2, 5, 5, 6, 6};
+        new P26RemoveDuplicatesFromSortedArray().solution(nums);
+        Arrays.toString(nums);
+    }
+
+}
