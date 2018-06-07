cae2111461f23e8c36dfcf37745e62e7a4756267
yuanzhongyan <yuanzhongyan@xiaomi.com>
2018-06-07 22:38:29 +0800

    solve 3sum with 'sort + hash + 2sum'

--- src/leetcode1st/array/P15ThreeSum.java
+++ src/leetcode1st/array/P15ThreeSum.java
@@ -1,15 +1,48 @@
 package leetcode1st.array;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import org.junit.Test;
 
 // 3Sum
-public class P15 {
-	// 解法一：DP
+public class P15ThreeSum {
+
+    /**
+     * 排序 + 哈希表 + 2Sum
+     */
+    public List<List<Integer>> solution1(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int tmp = nums[left] + nums[right];
+                if (nums[i] + tmp > 0) {
+                    right--;
+                } else if (nums[i] + tmp < 0) {
+                    left++;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * TODO DP待验证和调试
+     */
     public List<List<Integer>> threeSum(int[] A) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         
