e0f6e3ddeff31c4c57bf365f32afdd1599c6c089
yuanzhongyan <yuanzhongyan@gmail.com>
2018-06-29 22:05:54 +0800

    AC leet P74 solution II: two binary search

--- src/leetcode1st/binarySearch/P74SearchMatrix.java
+++ src/leetcode1st/binarySearch/P74SearchMatrix.java
@@ -23,4 +23,50 @@ public class P74SearchMatrix {
 
         return false;
     }
+
+    public boolean solution(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0] == null) {
+            return false;
+        }
+
+        // find first row >= target
+        int[] row = null;
+        int ROW = matrix.length, COL = matrix[0].length;
+        int st = 0, ed = ROW - 1;
+        while (st + 1 < ed) {
+            int md = st + (ed - st)/2;
+            if (matrix[md][COL - 1] == target) {
+                st = md;
+            } else if (matrix[md][COL - 1] < target) {
+                st = md;
+            } else {
+                ed = md;
+            }
+        }
+
+        if (COL > 0 && matrix[st][COL - 1] >= target) {
+            row = matrix[st];
+        } else if (COL > 0 && matrix[ed][COL - 1] >= target) {
+            row = matrix[ed];
+        } else {
+            return false;
+        }
+
+        // binary search in row
+        st = 0;
+        ed = COL - 1;
+        while (st + 1 < ed) {
+            int md = st + (ed - st)/2;
+            if (row[md] == target) {
+                return true;
+            } else if (row[md] < target) {
+                st = md;
+            } else {
+                ed = md;
+            }
+        }
+
+        return (row[st] == target || row[ed] == target);
+    }
+
 }
