8a94a21b0726a9500f40f1d7531b26c2cbb47df1
yuanzhongyan <yuanzhongyan@gmail.com>
2018-07-15 00:00:40 +0800

    AC leet P154 Find Min in Rotated Array II

--- src/problems/binarysearch/leet/once/P154FindMinimumInRotatedSortedArrayII.java
+++ src/problems/binarysearch/leet/once/P154FindMinimumInRotatedSortedArrayII.java
@@ -1,4 +1,27 @@
-package leetcode1st.binarySearch;
+下、package leetcode1st.binarySearch;
 
 public class P154FindMinimumInRotatedSortedArrayII {
+
+    public int findMin(int[] A) {
+        if (A == null || A.length == 0) {
+            return Integer.MIN_VALUE;
+        }
+
+        // case1: A[0] < A[hi]
+        // case2: A[0] > A[hi] or A[0] < A[hi]
+        int lo = 0, hi = A.length - 1;
+        while (lo + 1 < hi) {
+            int md = lo + (hi - lo)/2;
+            if (A[md] < A[hi]) {
+                hi = md;
+            } else if (A[md] > A[hi]) {
+                lo = md;
+            } else {
+                // TODO 这是什么意思？
+                hi--;
+            }
+        }
+
+        return Math.min(A[lo], A[hi]);
+    }
 }
