436879bd4f66b4d6dac4c6b742f5912c40250180
yuanzhongyan <yuanzhongyan@gmail.com>
2018-07-16 23:31:06 +0800

    AC leet P4 Median of Two Sorted Arrays: merge sort (space optimization)

--- src/problems/binarysearch/leet/once/P4MedianOfTwoSortedArrays.java
+++ src/problems/binarysearch/leet/once/P4MedianOfTwoSortedArrays.java
@@ -15,8 +15,8 @@ public class P4MedianOfTwoSortedArrays {
         int len = lenA + lenB;
 
         // merge sort
-        int idxA = 0, idxB = 0, idxC = 0;
         int[] C = new int[len];
+        int idxA = 0, idxB = 0, idxC = 0;
         // case1: both A and B have elements
         while (idxA < lenA && idxB < lenB) {
             if (A[idxA] < B[idxB]) {
@@ -41,4 +41,73 @@ public class P4MedianOfTwoSortedArrays {
             return C[len/2];
         }
     }
+
+    /**
+     * space optimization
+     */
+    public double solution2(int[] A, int[] B) {
+        if ((A == null || A.length == 0) && (B == null || B.length == 0)) {
+            return -1.0;
+        }
+
+        int lenA = (A == null) ? 0 : A.length;
+        int lenB = (B == null) ? 0 : B.length;
+        int len = lenA + lenB;
+        int m1 = 0, m2 = 0;
+
+        // merge sort
+        int idxA = 0, idxB = 0, idxC = 0;
+        while (idxA < lenA && idxB < lenB) {
+            if (idxC > (len/2)) {
+                break;
+            }
+            if (idxC == (len/2 - 1)) {
+                m1 = Math.min(A[idxA], B[idxB]);
+            }
+            if (idxC == (len/2)) {
+                m2 = Math.min(A[idxA], B[idxB]);
+            }
+            if (A[idxA] < B[idxB]) {
+                idxA++;
+            } else {
+                idxB++;
+            }
+            idxC++;
+        }
+
+        while (idxA < lenA) {
+            if (idxC > (len/2)) {
+                break;
+            }
+            if (idxC == (len/2 - 1)) {
+                m1 = A[idxA];
+            }
+            if (idxC == (len/2)) {
+                m2 = A[idxA];
+            }
+            idxA++;
+            idxC++;
+        }
+
+        while (idxB < lenB) {
+            if (idxC > (len/2)) {
+                break;
+            }
+            if (idxC == (len/2 - 1)) {
+                m1 = B[idxB];
+            }
+            if (idxC == len/2) {
+                m2 = B[idxB];
+            }
+            idxB++;
+            idxC++;
+        }
+
+        if (len % 2 == 0) {
+            return (m1 + m2)/2.0;
+        } else {
+            return m2;
+        }
+    }
+
 }
