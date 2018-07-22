23f7ed15364d53b1a3e76a8b920330b023e5f839
yuanzhongyan <yuanzhongyan@gmail.com>
2018-07-22 23:31:14 +0800

    AC leet P69 sqrt(x): binary search

--- src/problems/binarysearch/leet/once/P69Sqrtx.java
+++ src/problems/binarysearch/leet/once/P69Sqrtx.java
@@ -3,6 +3,26 @@ package leetcode1st.binarySearch;
 public class P69Sqrtx {
 
     public int mySqrt(int x) {
-        
+        if (x < 0) {
+            return -1;
+        }
+        if (x == 0) {
+            return 0;
+        }
+
+        int lo = 1, hi = x;
+        long md = 0;
+        while (lo + 1 < hi) {
+            md = lo + (hi - lo)/2;
+            if (md * md == x) {
+                return (int) md;
+            } else if (md * md < x) {
+                lo = (int) md;
+            } else {
+                hi = (int) md;
+            }
+        }
+
+        return lo;
     }
 }
