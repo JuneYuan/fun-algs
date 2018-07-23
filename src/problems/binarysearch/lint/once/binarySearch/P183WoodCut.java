498cb2c20adf33f2823af59b1011f6d7aa3efa03
yuanzhongyan <yuanzhongyan@gmail.com>
2018-07-23 23:55:09 +0800

    AC lint 183 wood cut: typical binary search

--- src/problems/binarysearch/lint/once/binarySearch/P183WoodCut.java
+++ src/problems/binarysearch/lint/once/binarySearch/P183WoodCut.java
@@ -1,4 +1,38 @@
 package lintcode1st.binarySearch;
 
 public class P183WoodCut {
+
+    public int woodCut(int[] L, int k) {
+        if (L == null || L.length == 0) {
+            return 0;
+        }
+
+        int lo = 0, hi = Integer.MIN_VALUE;
+        // get the upper bound of L
+        for (int h : L) {
+            if (h > hi) {
+                hi = h + 1;
+            }
+        }
+
+        while (lo + 1 < hi) {
+            int md = lo + (hi - lo)/2;
+            if (feasible(L, k, md)) {
+                lo = md;
+            } else {
+                hi = md;
+            }
+        }
+
+        return lo;
+    }
+
+    private boolean feasible(int[] L, int k, int x) {
+        int sum = 0;
+        for (int h : L) {
+            sum += h / x;
+        }
+
+        return sum >= k;
+    }
 }
