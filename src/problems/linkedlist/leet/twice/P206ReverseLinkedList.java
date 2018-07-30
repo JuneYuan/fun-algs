89ff3d31c02479f1e0f2a41f030707fbaf5e1536
yuanzhongyan <yuanzhongyan@gmail.com>
2018-07-31 00:02:57 +0800

    AC leet P206 ReverseLinkedList non-recursively

--- src/problems/linkedlist/leet/twice/P206ReverseLinkedList.java
+++ src/problems/linkedlist/leet/twice/P206ReverseLinkedList.java
@@ -1,4 +1,17 @@
 package problems.linkedlist.leet.twice;
 
 public class P206ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        List prev = null, curr = head;
+        while (curr != null) {
+            ListNode tmp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+
+        return prev;
+    }
+
 }
