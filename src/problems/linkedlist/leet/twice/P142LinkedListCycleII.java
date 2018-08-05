a382be0c40b82d19ea1e2aa9a95bb71f0fdb2233
yuanzhongyan <yuanzhongyan@gmail.com>
2018-08-05 19:28:56 +0800

    AC leet P142 Linked List Cycle II

--- src/problems/linkedlist/leet/twice/P142LinkedListCycleII.java
+++ src/problems/linkedlist/leet/twice/P142LinkedListCycleII.java
@@ -1,4 +1,27 @@
 package problems.linkedlist.leet.twice;
 
 public class P142LinkedListCycleII {
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = head;
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return fast;
+            }
+        }
+
+        return null;
+    }
+
 }
