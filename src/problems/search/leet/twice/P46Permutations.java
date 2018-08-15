6914c4450dce322f2ce9ef6f127e6897f5a3e478
yuanzhongyan <yuanzhongyan@gmail.com>
2018-08-15 15:19:10 +0800

    AC leet P46 Permutations (with iteration)

--- src/problems/search/leet/twice/P46Permutations.java
+++ src/problems/search/leet/twice/P46Permutations.java
@@ -1,6 +1,7 @@
 package problems.search.leet.twice;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,8 +9,11 @@ import org.junit.Test;
 
 public class P46Permutations {
 
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<List<Integer>();
+    /**
+     * Recursion (using subsets template)
+     */
+    public List<List<Integer>> solution1(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
         if (nums == null || nums.length == 0) {
             return result;
         }
@@ -31,9 +35,108 @@ public class P46Permutations {
                 continue;
             }
             list.add(nums[i]);
-            dfs(nums.list, result);
+            dfs(nums, list, result);
 
             list.remove(list.size() - 1);
+        }
+    }
+
+
+    /**
+     * Recursion
+     */
+    public List<List<Integer>> solution2(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<Integer> list = new ArrayList<>();
+
+        if (nums == null) {
+            return result;
+        } else {
+            // convert int[] to List<Integer>
+            for (int item : nums) {
+                list.add(item);
+            }
+        }
+
+        if (nums.length <= 1) {
+            result.add(list);
+            return result;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int[] numsNew = new int[nums.length - 1];
+            System.arraycopy(nums, 0, numsNew, 0, i);
+            System.arraycopy(nums, i + 1, numsNew, i, nums.length - i - 1);
+
+            List<List<Integer>> resTemp = solution2(numsNew);
+            for (List<Integer> temp : resTemp) {
+                temp.add(nums[i]);
+                result.add(temp);
+            }
+        }
+
+        return result;
+    }
+
+
+    /**
+     * Iteration
+     */
+    public List<List<Integer>> solution3(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+
+        // deep copy (do not change nums)
+        int[] perm = Arrays.copyOf(nums, nums.length);
+        // sort first!!!
+        Arrays.sort(perm);
+
+        while (true) {
+            // step0: add perm into result
+            List<Integer> tmpList = new ArrayList<Integer>();
+            for (int i : perm) {
+                tmpList.add(i);
+            }
+            result.add(tmpList);
+
+            // step1: search the first perm[k] < perm[k+1] backward
+            int k = -1;
+            for (int i = perm.length - 2; i >= 0; i--) {
+                if (perm[i] < perm[i - 1]) {
+                    k = i;
+                    break;
+                }
+            }
+            // if current rank is the largest, exit while loop
+            if (k == -1) {
+                break;
+            }
+
+            // step2: search the first perm[k] < perm[l] backward
+            int l = perm.length - 1;
+            while (l > k && perm[l] <= perm[k]) {
+                l--;
+            }
+
+            // step3: swap perm[k] with perm[l]
+            int tmp = perm[k];
+            perm[k] = perm[l];
+            perm[l] = tmp;
+
+            // step4: reverse between k+1 and perm.length-1;
+            reverse(perm, k + 1, perm.length - 1);
+        }
+
+        return result;
+    }
+
+    private void reverse(int[] nums, int lb, int ub) {
+        for (int i = lb, j = ub; i < j; i++, j--) {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
         }
     }
 
