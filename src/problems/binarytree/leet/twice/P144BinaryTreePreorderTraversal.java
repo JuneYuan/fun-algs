62498b5d0d2459e6b93d4e7ee112ab35a3d89fa1
yuanzhongyan <yuanzhongyan@gmail.com>
2018-08-28 00:27:36 +0800

    AC leet P144 (divide and conquer; traversal; iterative)

--- src/problems/binarytree/leet/twice/P144BinaryTreePreorderTraversal.java
+++ src/problems/binarytree/leet/twice/P144BinaryTreePreorderTraversal.java
@@ -7,7 +7,42 @@ import java.util.List;
 
 public class P144BinaryTreePreorderTraversal {
 
-    public List<Integer> solution(TreeNode root) {
+    /**
+     * solution1: divide and conquer
+     */
+    public List<Integer> solution1(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root != null) {
+            // divide
+            List<Integer> left = solution1(root.left);
+            List<Integer> right = solution1(root.right);
+            // merge
+            result.add(root.val);
+            result.addAll(left);
+            result.addAll(right);
+        }
+
+        return result;
+    }
+
+    /**
+     * solution2: traversal
+     */
+    private List<Integer> result = new ArrayList<>();
+    public List<Integer> solution2(TreeNode root) {
+        if (root != null) {
+            result.add(root.val);
+            solution2(root.left);
+            solution2(root.right);
+        }
+
+        return result;
+    }
+
+    /**
+     * solution3: 迭代
+     */
+    public List<Integer> solution3(TreeNode root) {
         List<Integer> result = new ArrayList<>();
 
         Deque<TreeNode> stack = new ArrayDeque<>();
