f3731e8e1b68b4dd30c525739519ccf6b92c61fa
yuanzhongyan <yuanzhongyan@gmail.com>
2018-09-23 23:42:18 +0800

    solve geeksforgeeks: Diameter of a Binary Tree

---
+++ src/problems/binarytree/geeks/DiameterOfBinaryTree.java
@@ -0,0 +1,40 @@
+package problems.binarytree.leet.twice;
+
+public class DiameterOfBinaryTree {
+
+    public int diameter(TreeNode root) {
+        if (root == null)   return 0;
+
+        // left, right height
+        int lH = getHeight(root.left);
+        int rH = getHeight(root.right);
+
+        // left, right subtree diamerter
+        int lDia = diameter(root.left);
+        int rDia = diameter(root.right);
+
+        int maxSubDia = Math.max(lDia, rDia);
+        return Math.max(maxSubDia, lH + rH + 1);
+    }
+
+    private int getHeight(TreeNode root) {
+        if (root == null)   return 0;
+
+        return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.left.right.left = new TreeNode(6);
+        root.left.right.left.right = new TreeNode(7);
+        root.left.left.left = new TreeNode(8);
+
+        Solution sol = new Solution();
+        int maxDistance = sol.diameter(root);
+        System.out.println("Max Distance: " + maxDistance);
+    }
+}
