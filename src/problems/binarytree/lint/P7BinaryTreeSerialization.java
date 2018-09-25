f2c1449bdc01bdc2600ff971473f596c4ee80f17
yuanzhongyan <yuanzhongyan@gmail.com>
2018-09-25 23:32:53 +0800

    complete lint P7

--- src/problems/binarytree/lint/P7BinaryTreeSerialization.java
+++ src/problems/binarytree/lint/P7BinaryTreeSerialization.java
@@ -1,4 +1,54 @@
 package problems.binarytree.lint;
 
+import java.util.StringTokenizer;
+
 public class P7BinaryTreeSerialization {
+
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        if (root == null) {
+            return sb.toString();
+        }
+
+        seriaHelper(root, sb);
+
+        return sb.substring(0, sb.length() - 1);
+    }
+
+    private void seriaHelper(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            sb.append("#,");
+        } else {
+            sb.append(root.val).append(",");
+            seriaHelper(root.left, sb);
+            seriaHelper(root.right, sb);
+        }
+    }
+
+    public TreeNode deserialize(String data) {
+        if (data == null || data.length() == 0) {
+            return null;
+        }
+
+        StringTokenizer st = new StringTokenizer(data, ",");
+        return deseriaHelper(st);
+    }
+
+    private TreeNode deseriaHelper(StringTokenizer st) {
+        if (!st.hasMoreTokens()) {
+            return null;
+        }
+
+        String val = st.nextToken();
+        if (val.equals("#")) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.parseInt(val));
+        root.left = deseriaHelper(st);
+        root.right = deseriaHelper(st);
+
+        return root;
+    }
+
 }
